6cdc6df8f40f00a8fc6da0b442c896bdef2d5f36
Stelian <stoianstelian33@gmail.com>
2021-04-19 15:13:12 +0300

    added react-router-dom

--- muse_client/src/App.tsx
+++ muse_client/src/App.tsx
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { TestClass } from './components/class_boilerplate';
+import { Player } from './components/spotify_player';
+import { BrowserRouter as Router,
+  Switch,
+  Route,
+ } from 'react-router-dom';
+ 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Switch>
+            <Route path="/test">
+              <TestClass />
+            </Route>
+            <Route path="/player">
+              <Player uri="spotify:track:1nJE8TEWK9hf9Bl0pekJCi"/>
+            </Route>
+            <Route path="/">
+              <img src={logo} className="App-logo" alt="logo" />
+            </Route>
+          </Switch>
+        </Router>
       </header>
     </div>
   );
---
+++ muse_client/src/components/class_boilerplate.tsx
@@ -0,0 +1,42 @@
+/**
+ * Example module created as a stateful class example
+ * for using typescript with react.
+ */
+
+import { Component } from 'react';
+
+/**
+ * State type of the class.
+ */
+interface TestState {
+
+}
+
+/**
+ * Prop type of the class.
+ */
+interface TestProps {
+
+}
+
+/**
+ * This class exists as a boilerplate for future stateful typescript classes.
+ */
+export class TestClass extends Component<TestProps, TestState> {
+    // Declaram state
+    constructor(props: TestProps) {
+        super(props);
+        this.state = {
+            
+        };
+    }
+    
+    render() {
+        return(
+            <div>
+                <h3> This is an example </h3>
+            </div>
+        );
+    }; 
+
+}
