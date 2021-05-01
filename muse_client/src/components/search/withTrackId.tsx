d674c316c1b031a5465b837e69b9470e27acdcbb
Stelian <stoianstelian33@gmail.com>
2021-05-01 23:56:45 +0300

    WithTrackId Hoc

--- muse_client/src/components/search/withTrackId.tsx
+++ muse_client/src/components/search/withTrackId.tsx
@@ -1,2 +1,29 @@
-import { Component } from 'react';
+import React from 'react';
 
+interface WithTrackIdProps {
+    track_id: string,
+    getId: (id: string) => void,
+}
+
+export function withTrackId<P, T extends WithTrackIdProps>(Component: React.ComponentType<T>) {
+    return class extends React.Component<T & WithTrackIdProps> {
+
+        constructor(props: T & P) {
+            super(props);
+
+            this.handleClick = this.handleClick.bind(this);
+        }
+
+        handleClick() {
+            this.props.getId(this.props.track_id);
+        }
+
+        render() {
+            return ( 
+                <div onClick={this.handleClick}>
+                    <Component {...this.props as T} />
+                </div>
+            );
+        }
+    }
+}
